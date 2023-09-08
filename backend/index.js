44ed2624acac5f5ab0abe70ccf56d01404b334ce
Artiom Tsimkin <artiomtz@gmail.com>
2023-09-08 00:24:54 -0400

    added cors

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,30 @@
-// const cors = require("cors"); //  npm install cors
+const cors = require("cors");
 const db = require("./database/access");
 const express = require("express");
 const app = express();
 const telemetryRouter = require("./routes/telemetry");
 require("dotenv").config();
 
-// app.use(
-//     cors({
-//       origin: clientRoute,
-//       methods: "GET,HEAD,PUT,POST,DELETE",
-//       credentials: true,
-//     })
-//   );
+const allowedOrigins = [
+  process.env.FRONTEND_DEV_URL,
+  process.env.FRONTEND_PROD_URL,
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      console.log("Request from origin:", origin);
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        console.warn("Not allowed origin:", origin);
+      }
+    },
+  })
+);
 
 const PORT = process.env.PORT || 5000;
+app.use(cors());
 app.use(express.json());
 app.use("/telemetry", telemetryRouter);
 app.listen(PORT);
